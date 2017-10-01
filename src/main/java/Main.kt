bd77c76b51d447d9d39979e6b3a37266aa669faf
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-01 17:31:45 +0200

    (music) Weeee are the functors my frieeeend

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -5,3 +5,8 @@ sealed class Result<out A, out E>
 data class Success<out A, out E>(val value: A) : Result<A, E>()
 
 data class Failure<out A, out E>(val error: E) : Result<A, E>()
+
+fun <A, E, B> Result<A, E>.map(transform: (A) -> B): Result<B, E> = when(this) {
+    is Success -> Success(transform(value))
+    is Failure -> Failure(error)
+}
